05fa55ee80332087e3bc5d881aa7d319add83a5a
hediye <hediyehghafari80@gmail.com>
2025-12-18 15:25:02 +0330

    change main.js

--- main.js
+++ main.js
@@ -6,22 +6,24 @@ const NotFoundHandler = require("./src/common/exception/not-found.handler");
 const AllExceptionHandler = require("./src/common/exception/all-exception.handler");
 const cors = require("cors");
 dotenv.config();
-async function main () {
-    const app = express();
-    const port = process.env.PORT;
-    require("./src/config/mongoose.config");
-    app.use(cors({
-        origin: "*"
-    }));
-    app.use(express.json());
-    app.use(express.urlencoded({extended: true}));
-    app.use(express.static("public"));
-    app.use(mainRouter);
-    SwaggerConfig(app);
-    NotFoundHandler(app);
-    AllExceptionHandler(app);
-    app.listen(port, () => {
-        console.log(`server: http://localhost:${port}`);
-    });
+async function main() {
+  const app = express();
+  const port = process.env.PORT || 10000;
+  require("./src/config/mongoose.config");
+  app.use(
+    cors({
+      origin: "*",
+    })
+  );
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static("public"));
+  app.use(mainRouter);
+  SwaggerConfig(app);
+  NotFoundHandler(app);
+  AllExceptionHandler(app);
+  app.listen(port, "0.0.0.0", () => {
+    console.log(`server running on port ${port}`);
+  });
 }
 main();
